c383220048d43bff809fc5ac4dfbdf76f06fa200
JR <jrfrazier21@gmail.com>
2017-10-17 10:52:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(string)
  if (string == "I LOVE YOU GRANDMA!")
-    return "NO, NOT SINCE 1938!"
-  elsif (string.upcase == true)
     return "I LOVE YOU TO PUMPKIN!"
+  elsif (string.upcase == true)
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
